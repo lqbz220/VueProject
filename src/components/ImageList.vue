35432c5c964179fb04b07df35dd09d688a8180f7
Lei Shi <lshi@docnetwork.org>
2023-02-26 23:53:19 -0500

    WIP imageInfo empty

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -9,9 +9,14 @@ const imageStore = useImageStore();
 <template>
   <v-sheet class="bg-black">
     <v-row>
-      <v-col v-for="image in imageStore.images" :key="image.id" class="d-flex child-flex" cols="4">
+      <v-col class="d-flex child-flex" cols="4">
         <v-sheet class="pa-2 ma-2">
-          <SingleImage :image="image" />
+          <SingleImage
+          v-for="(image, index) in imageStore.images"
+          :image="image"
+          :index="index"
+          :key="image.id"
+          />
         </v-sheet>
         <template v-slot:placeholder>
           <v-row class="d-flex justify-center fill-height ma-1 align-center">
