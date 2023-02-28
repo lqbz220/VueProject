85ba2f0be954c5fb69994849264e2055f0622bb0
Lei Shi <lshi@docnetwork.org>
2023-02-28 10:32:31 -0500

    adds description

--- src/stores/imageStore.js
+++ src/stores/imageStore.js
@@ -3,15 +3,15 @@ import { defineStore } from "pinia";
 export const useImageStore = defineStore("imageStore", {
   state: () => ({
     images: [
-      { id: 1, imageURL: "https://picsum.photos/200", description: "good" },
-      { id: 2, imageURL: "https://picsum.photos/201", description: "great" },
-      { id: 3, imageURL: "https://picsum.photos/202", description: "great" },
-      { id: 4, imageURL: "https://picsum.photos/203", description: "great" },
-      { id: 5, imageURL: "https://picsum.photos/204", description: "great" },
-      { id: 6, imageURL: "https://picsum.photos/205", description: "great" },
-      { id: 7, imageURL: "https://picsum.photos/206", description: "great" },
-      { id: 8, imageURL: "https://picsum.photos/207", description: "great" },
-      { id: 9, imageURL: "https://picsum.photos/208", description: "great" },
+      { id: 1, imageURL: "https://picsum.photos/200", description: "A good image" },
+      { id: 2, imageURL: "https://picsum.photos/201", description: "Beautify picture" },
+      { id: 3, imageURL: "https://picsum.photos/202", description: "A great image" },
+      { id: 4, imageURL: "https://picsum.photos/203", description: "How pretty" },
+      { id: 5, imageURL: "https://picsum.photos/204", description: "Summer time" },
+      { id: 6, imageURL: "https://picsum.photos/205", description: "An empty space" },
+      { id: 7, imageURL: "https://picsum.photos/206", description: "A good image" },
+      { id: 8, imageURL: "https://picsum.photos/207", description: "Winter snow" },
+      { id: 9, imageURL: "https://picsum.photos/208", description: "The city" },
     ],
   }),
   actions: {
