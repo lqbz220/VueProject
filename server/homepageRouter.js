761c1dcfd490e5b13246daa329bed17af3e8be74
Lei Shi <lshi@docnetwork.org>
2023-05-21 20:30:13 -0400

    Fixes html engine

--- server/homepageRouter.js
+++ server/homepageRouter.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/*", (_req, res) => {
-  res.sendFile("/index.html");
+  res.render("index");
 });
 
 export default router;
--- server/index.js
+++ server/index.js
@@ -2,19 +2,23 @@ import express, { json } from 'express';
 import homepageRouter from "./homepageRouter.js";
 import cors from 'cors';
 import api from './api/index.js';
-// import arts from './api/arts';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+
 const app = express();
 const PORT = 8000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = join(__filename, '..');
 
 // middleware
 app.use(cors())
 app.use(json())
-// app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
+app.set('views', join(__dirname, 'views'));
+
 // api routes
 app.use('/api', api);
-// app.get("/api/v1/hello", (_req, res) => {
-//   res.json({ message: "Hello, world!" });
-// });
 
 app.use(homepageRouter);
 
